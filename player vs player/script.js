fa3b0e2387b4e2d120e7bf35e0036ab7814ea987
Harry Garofani <harrygarofani@Harrys-MacBook-Air.local>
2019-06-21 12:43:27 +1000

    updates

--- player vs player/script.js
+++ player vs player/script.js
@@ -1,57 +1,81 @@
-document.getElementById("go").addEventListener("click", startAll)
+var myTurn = Math.floor(Math.random() * 2);
+var gameOver = false
+document.getElementById("go").addEventListener("click", playFunction);
 
-function startAll() {
-  var a1 = 0
-  var a2 = 0
-  var a3 = 0
-
-  var b1 = 0
-  var b2 = 0
-  var b3 = 0
-
-  var c1 = 0
-  var c2 = 0
-  var c3 = 0
-
-  var myTurn = Math.floor(Math.random() * 2)
-  var xoTurn = ""
-
-  document.getElementById("a1").innerHTML = "1"
-  document.getElementById("a2").innerHTML = "2"
-  document.getElementById("a3").innerHTML = "3"
-  document.getElementById("b1").innerHTML = "4"
-  document.getElementById("b2").innerHTML = "5"
-  document.getElementById("b3").innerHTML = "6"
-  document.getElementById("c1").innerHTML = "7"
-  document.getElementById("c2").innerHTML = "8"
-  document.getElementById("c3").innerHTML = "9"
+function playFunction() {
+  document.getElementById("a1").innerHTML = "-";
+  document.getElementById("a2").innerHTML = "-";
+  document.getElementById("a3").innerHTML = "-";
+  document.getElementById("b1").innerHTML = "-";
+  document.getElementById("b2").innerHTML = "-";
+  document.getElementById("b3").innerHTML = "-";
+  document.getElementById("c1").innerHTML = "-";
+  document.getElementById("c2").innerHTML = "-";
+  document.getElementById("c3").innerHTML = "-";
+  document.getElementById("a1").style.background = "#636466";
+  document.getElementById("a2").style.background = "#636466";
+  document.getElementById("a3").style.background = "#636466";
+  document.getElementById("b1").style.background = "#636466";
+  document.getElementById("b2").style.background = "#636466";
+  document.getElementById("b3").style.background = "#636466";
+  document.getElementById("c1").style.background = "#636466";
+  document.getElementById("c2").style.background = "#636466";
+  document.getElementById("c3").style.background = "#636466";
 
   if (myTurn == 0) {
-    xoTurn = "X"
+    document.getElementById("turn").innerHTML = "It is Noughts' turn";
   } else {
-    xoTurn = "O"
+    document.getElementById("turn").innerHTML = "It is Cross's turn";
+  }
+}
+function clickFunction(position) {
+  if(gameOver == false){
+  if (document.getElementById(position).innerHTML != ("X" || "0")) {
+    if (myTurn == 1) {
+      document.getElementById(position).innerHTML = "X";
+      myTurn = 0;
+      document.getElementById("turn").innerHTML = "It is Noughts' turn"
+    } else {
+      document.getElementById(position).innerHTML = "0";
+      myTurn = 1;
+      document.getElementById("turn").innerHTML = "It is Cross's turn"
+    }
   }
 
-  document.getElementById("turn").innerHTML = "It is " + xoTurn + "'s turn."
+  var a1Val = document.getElementById("a1").innerHTML;
+  var a2Val = document.getElementById("a2").innerHTML;
+  var a3Val = document.getElementById("a3").innerHTML;
+  var b1Val = document.getElementById("b1").innerHTML;
+  var b2Val = document.getElementById("b2").innerHTML;
+  var b3Val = document.getElementById("b3").innerHTML;
+  var c1Val = document.getElementById("c1").innerHTML;
+  var c2Val = document.getElementById("c2").innerHTML;
+  var c3Val = document.getElementById("c3").innerHTML;
 
-  document.getElementById("a1").addEventListener("click", a1Click)
-
-  function a1Click() {
-    if(a1 == 1){
-    alert("This square is already taken.")
-    } else if (a1 == 0 && myTurn == 0) {
-      document.getElementById("a1").innerHTML = "X"
-      a1 = 1
-      myTurn = 1
-      xoTurn = "O"
-        document.getElementById("turn").innerHTML = "It is " + xoTurn + "'s turn."
-    } else if (a1 == 0 && myTurn == 1) {
-      document.getElementById("a1").innerHTML = "O"
-      a1 = 2
-      myTurn = 0
-      xoTurn = "X"
-        document.getElementById("turn").innerHTML = "It is " + xoTurn + "'s turn."
-    } 
+  if (a1Val == a2Val && a2Val == a3Val && a1Val == ("X" || "0")) {
+    winFtn("a1", "a2", "a3");
+  } else if (b1Val == b2Val && b2Val == b3Val && b1Val == ("X" || "0")) {
+    winFtn("b1", "b2", "b3");
+  } else if (c1Val == c2Val && c2Val == c3Val && c1Val == ("X" || "0")) {
+    winFtn("c1", "c2", "c3");
+  } else if (a1Val == b1Val && b1Val == c1Val && a1Val == ("X" || "0")) {
+    winFtn("a1", "b1", "c1");
+  } else if (a3Val == b2Val && b2Val == c1Val && a3Val == ("X" || "0")) {
+    winFtn("a3", "b2", "c1");
+  } else if (a3Val == b3Val && b3Val == c3Val && a3Val == ("X" || "0")) {
+    winFtn("a3", "b3", "c3");
+  } else if (a1Val == b2Val && b2Val == c3Val && a1Val == ("X" || "0")) {
+    winFtn("a1", "b2", "c3");
+  } else if (a3Val == b2Val && b2Val == c1Val && a3Val == ("X" || "0")) {
+    winFtn("a3", "b2", "c1");
   }
-
+  function winFtn(a, b, c) {
+    document.getElementById(a).style.background = "#063852";
+    document.getElementById(b).style.background = "#063852";
+    document.getElementById(c).style.background = "#063852";
+    alert(document.getElementById(a).innerHTML + " WINS");
+    document.getElementById("turn").innerHTML = "Press Start to play again."
+    gameOver = true
+  }
+}
 }
